1450f47846d94c36bc4aae031b01ed0fc497359d
lujain415 <lujainalaamri55@gmail.com>
2025-06-22 14:08:41 +0300

    completed

---
+++ main.py
@@ -0,0 +1,33 @@
+phone_book=[
+    {"name":"Amal",
+    "number":"0568323222"},
+    {"name":"Mohammed",
+    "number":"0522222232"},
+    {"name":"Khadijah",
+     "number":"0532335983"},
+     {"name": "Abdullah",
+      "number":"0545341144"},
+      {"name":"Rawan",
+       "number":"0545534556"},
+       {"name": "Faisal",
+        "number":"0560664566"},
+        {"name":"Layla",
+         "number":"0567917077"}]
+n=input("input your number ")
+def find_number():
+   
+    for person in phone_book:
+         if person["number"] == n:
+             print(f"the number for {person['name']} ")
+             return
+    else:
+        print("Sorry, the number is not found")
+
+def vaild_num():
+
+    if len(n)!=10 or not n.isdigit():
+        print("This is invalid number")
+        return
+
+find_number()
+vaild_num()
